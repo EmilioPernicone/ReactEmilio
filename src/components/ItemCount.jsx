0438709c4bc44b8659537f14e713fb793e38ad4a
Pernicone Emilio <emiliopernicone@gmail.com>
2022-09-13 21:15:49 -0300

    Contador con boton

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const ItemCount = ({initial, stock, onAdd}) => {
+    const [count,setCount]= useState(initial)
+    const increase =() => count < stock && setCount(count + 1)
+    const decrease =() => count > initial && setCount(count - 1)
+    return(
+      <>
+       <button onClick={decrease}>-</button>
+       <h2>{count}</h2>
+       <button onClick={increase}>+</button>
+       <button>Agregar al carrito</button>
+      </>
+    )
+}
+export default ItemCount;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ItemCount from './ItemCount';
 
 const ItemListContainer = ({greeting}) => {
   return (
-   <h1>Benvenidos, {greeting}</h1>
+    <>
+      <h1>Benvenidos, {greeting}</h1>
+      <ItemCount initial={1} stock={5} onAdd={() => {}} />
+    </>
+
   )
 }
 export default ItemListContainer;
